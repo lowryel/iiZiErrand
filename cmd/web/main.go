35628b83da27657da7f5905fa79e41ed7f333e4c
Eugene Agbaglo <eugeneagbaglo@gmail.com>
2024-11-28 10:24:26 +0000

    Update main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -86,10 +86,10 @@ func main() {
 	
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
-		PORT = "5500" // Default port if not specified in the environment
+		PORT = "11750" // Default port if not specified in the environment
 	}
 	// serve on port 3000
-	err = app.Listen(PORT)
+	err = app.Listen(":"+PORT)
 	if err != nil {
 		errorLogger.Println(err)
 	}
